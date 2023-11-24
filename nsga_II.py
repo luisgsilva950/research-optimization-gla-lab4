138388af06710264bc28763aa5dcaf4ac39f2c6c
luisdomingues <luisgsilva950@gmail.com>
2023-11-23 22:08:39 -0300

    fix nsga

--- nsga_II.py
+++ nsga_II.py
@@ -9,27 +9,59 @@ from models import Customer, PA, Coordinate
 from utils import DISTANCES
 
 
+def get_customers() -> List[Customer]:
+    customers = []
+    with open('clientes.csv') as file:
+        content = file.readlines()
+        for index, row in enumerate(content):
+            row = row.split(",")
+            customer = Customer(coordinates=Coordinate(x=float(row[0]), y=float(row[1])),
+                                consume=float(row[2]),
+                                index=index)
+            customers.append(customer)
+    return customers
+
+
+def get_pas() -> List[PA]:
+    points = []
+    for x in range(0, 1010, 10):
+        for y in range(0, 1010, 10):
+            points.append(PA(x=x, y=y, index=len(points)))
+    return points
+
+
+PAS = get_pas()
+CUSTOMERS = get_customers()
+
+for c in CUSTOMERS:
+    for p in PAS:
+        if DISTANCES[c.index][p.index] < 85:
+            p.possible_customers.append(c)
+
+
 class NSGAII:
     min_customers_attended = 570
     max_distance = 85
     max_active_points = 100
     max_consumed_capacity = 150
 
-    def __init__(self, customers: List[Customer], points: List[PA],
-                 solution=None, active_points=None):
+    def __init__(self, customers: List[Customer], points: List[PA], solution=None, priorities=None):
         self.customers = customers or []
         self.points = points or []
-        self.active_points = active_points or set()
         self.solution = solution
         self.total_distance = 0
         self.pas_count = 0
-        self.priorities = np.random.permutation(len(self.points))
+        self.priorities = priorities
 
-        print(self.priorities)
+        # print(self.priorities)
 
     def get_solution(self, priorities: List[int] = None):
+        if self.solution:
+            return self
+
         if not priorities:
             priorities = np.random.permutation(len(self.points))
+
         count_pas, total_distance = 0, 0
         solution = [[] for _ in range(len(self.points))]
         customers_attended = set()
@@ -55,16 +87,13 @@ class NSGAII:
                 continue
             count_pas += 1
             customers_count += len(l)
-            print(idx, l, "\n")
 
-        print("Numero de pontos", count_pas, "Taxa de clientes atendidos", customers_count / len(self.customers),
-              "Distância total", total_distance)
-
+        self.priorities = priorities
         self.pas_count = count_pas
         self.total_distance = total_distance
         self.solution = solution
 
-        return count_pas, total_distance, solution
+        return self
 
     def plot(self, idx: int = None):
         plotter = GraphicPlotter(f'NSGA Solution {idx or random.randint(0, 100)}', connexions=[
@@ -75,27 +104,7 @@ class NSGAII:
 
     @staticmethod
     def from_csv() -> 'NSGAII':
-
-        def get_customers() -> List[Customer]:
-            customers = []
-            with open('clientes.csv') as file:
-                content = file.readlines()
-                for index, row in enumerate(content):
-                    row = row.split(",")
-                    customer = Customer(coordinates=Coordinate(x=float(row[0]), y=float(row[1])),
-                                        consume=float(row[2]),
-                                        index=index)
-                    customers.append(customer)
-            return customers
-
-        def get_pas() -> List[PA]:
-            points = []
-            for x in range(0, 1010, 10):
-                for y in range(0, 1010, 10):
-                    points.append(PA(x=x, y=y, index=len(points)))
-            return points
-
-        return NSGAII(customers=get_customers(), points=get_pas())
+        return NSGAII(customers=CUSTOMERS, points=PAS)
 
     def plot_customers(self):
         import matplotlib.pyplot as plt
@@ -121,80 +130,83 @@ class NSGAII:
         plt.show()
 
     def mutation(self):
+        if random.random() > 0.7:
+            return
+
         N = len(self.priorities)
         mutate_priorities = self.priorities
 
-        for i in range(N):
-            if(random.random() < 0.7): #mutation prob = 0.7
-                j = random.sample(range(N),1)
-                mutate_priorities[i], mutate_priorities[j] = mutate_priorities[j], mutate_priorities[i]
-        
+        n_exchanges = random.randint(1, 6)
+
+        for _ in range(n_exchanges):
+            i, j = random.randint(0, 100 - 1), random.randint(0, N - 1)
+            mutate_priorities[i], mutate_priorities[j] = mutate_priorities[j], mutate_priorities[i]
+
         self.priorities = mutate_priorities
 
 
-def dominate(solution1, solution2): #verifica se geracao1 domina geracao2
+def dominate(solution1, solution2):  # verifica se geracao1 domina geracao2
+    return (solution1.total_distance < solution2.total_distance and solution1.pas_count <= solution2.pas_count) or \
+        (solution1.total_distance <= solution2.total_distance and solution1.pas_count < solution2.pas_count)
 
-  if(solution1.total_distance < solution2.total_distance and solution1.active_points < solution2.active_points):
-    return True
 
-  else:
-    return False
- 
-def crossover(solutionA,solutionB):
+def crossover(solutionA, solutionB):
     N = len(solutionA.priorities)
-    C = [0]*N
+    C = [0] * N
 
     A = solutionA.priorities
     B = solutionB.priorities
 
-    for i in range(1,int(N/2)+1):
-        indice = A.index(i)
+    A_idxs = np.argsort(A)
+    B_idxs = np.argsort(B)
+
+    for i in range(1, int(N / 2)):
+        indice = A[A_idxs[i]]
         C[indice] = A[indice]
 
-    for i in range(int(N/2)+1, N+1):
-        indice = B.index(i)
-        if(C[indice] == 0):
+    for i in range(int(N / 2) + 1, N):
+        indice = B[B_idxs[i]]
+        if C[indice] == 0:
             C[indice] = B[indice]
-
         else:
             for j in range(N):
-                if(C[j] == 0):
+                if C[j] == 0:
                     C[j] = B[indice]
                     break
+    return C
 
-    nsga = NSGAII()
-    return nsga.get_solution(C)
 
-def get_next_generation_solutions(front): #get the first N/2 front elements to the next generation
+def get_next_generation_solutions(front):  # get the first N/2 front elements to the next generation
     next_gen_solutions = []
 
     for i in range(len(front)):
         for objeto in front[i]:
-            if(len(next_gen_solutions) < 50):
-                if(50 - len(next_gen_solutions) > len(front[i])):
+            if len(next_gen_solutions) < 50:
+                if 50 - len(next_gen_solutions) > len(front[i]):
                     next_gen_solutions.append(objeto)
                 else:
-                    next_gen_solutions = next_gen_solutions + (crowding_distance_(front[i], 50 - len(next_gen_solutions)))
-    
+                    next_gen_solutions = next_gen_solutions + (
+                        crowding_distance_(front[i], 50 - len(next_gen_solutions)))
+
     return next_gen_solutions
+
 
 def crowding_distance_(front_component, N):
     points = []
 
     for i in range(len(front_component)):
-        points.append((front_component[i].total_distance, front_component[i].active_points))
+        points.append((front_component[i].pas_count, front_component[i].pas_count))
 
     def crowding_distance(point, other_points):
-        distances = [np.sqrt((point[0] - p[0])**2 + (point[1] - p[1])**2) for p in other_points]
+        distances = [np.sqrt((point[0] - p[0]) ** 2 + (point[1] - p[1]) ** 2) for p in other_points]
         return sum(sorted(distances)[1:-1])
-    
+
     selected_points = []
 
     for _ in range(N):
         crowding_dict = {i: crowding_distance(point, points) for i, point in enumerate(points)}
         max_crowding_index = max(crowding_dict, key=crowding_dict.get)
         selected_points.append(points[max_crowding_index])
-        del points[max_crowding_index]
 
     selected_solutions = []
 
@@ -207,109 +219,81 @@ def crowding_distance_(front_component, N):
 
 if __name__ == '__main__':
     solution_number = 50
-    nsga = NSGAII.from_csv()
     generation = []
     first_pop = []
 
     for i in range(solution_number):
         s = NSGAII.from_csv()
+        s.get_solution()
         first_pop.append(s)
-    
+
+    print("Soluções iniciais geradas...")
     generation = []
 
     NSGAII_interactions = 50
 
     population = first_pop
 
-
-
     for count in range(NSGAII_interactions):
         front = []
 
+        # print("NSGAII_interactions")
+        children_priorities = []
         children = []
         for i in range(len(population)):
-            j, k = random.sample(range(0, len(population)),2)
-            children.append(crossover(population[j], population[k]))
+            # print("i_population")
+            j, k = random.sample(range(0, len(population)), 2)
+            children_priorities.append(crossover(population[j], population[k]))
 
-
-        for i in range(len(population)):
-            children[i].mutation()
+        for i in range(len(children_priorities)):
+            # print("i_children_priorities")
+            c = NSGAII(customers=CUSTOMERS, points=PAS, priorities=children_priorities[i])
+            c.mutation()
+            c.get_solution()
+            children.append(c)
 
         population = population + children
 
-        for i in range(len(population)):
-            population[i].get_solution()
-
-        while(len(population) > 0):
+        new_solutions_count = 0
+        while new_solutions_count < 100:
+            remove_idxs = set()
+            front_atual = []
             for i in range(len(population)):
-                front_atual = []
-
                 Sp = []
-                np = 0
+                non_p = 0
 
                 for j in range(len(population)):
-                    if(dominate(population[i],population[j])):
+                    if dominate(population[i], population[j]):
                         Sp.append(population[i])
-                    elif(dominate(population[j], population[i])):
-                        np += 1
+                    elif dominate(population[j], population[i]):
+                        non_p += 1
 
-                if(np == 0):
+                if non_p == 0:
+                    new_solutions_count += 1
+                    remove_idxs.add(i)
                     front_atual.append(population[i])
-            
+
+            population = [p for idx, p in enumerate(population) if idx not in remove_idxs]
+
             front.append(front_atual)
 
-            population = [elemento for elemento in population if elemento not in front_atual]
+        # for solutions in front:
+        #     import matplotlib.pyplot as plt
+        #
+        #     r = numpy.random.random()
+        #     b = numpy.random.random()
+        #     g = numpy.random.random()
+        #     color = (r, g, b)
+        #     for s in solutions:
+        #         plt.plot(s.pas_count, s.total_distance, '^', c=color)
+        #         plt.grid()
+        #
+        # plt.show()
 
         generation.append(front[0])
 
+        min_pas_count = min([n.pas_count for n in front[0]])
+        min_total_distance = min([n.total_distance for n in front[0]])
+        print([min_pas_count, min_total_distance])
+
         population = get_next_generation_solutions(front)
-
-
-
-    # start = time.time()
-    #
-    # for i in range(50):
-    #     s = nsga.get_initial_solution()
-    #     solutions.append(s)
-    #
-    # print("Initial solution", min([m.penal_fitness for m in solutions]), f"{time.time() - start} seconds")
-    #
-    # start = time.time()
-    #
-    # for _ in range(500):
-    #     size = len(solutions)
-    #
-    #     for i in range(size):
-    #         solution: NSGAII = solutions[i]
-    #         solution.k = random.randint(1, 3)
-    #         start = time.time()
-    #         # print("Shaking...")
-    #         solutions.append(solution.shake())
-    # print("Shake", f"{time.time() - start} seconds")
-
-    # print("Finish to generate mutations", f"{time.time() - start} seconds")
-
-    # start = time.time()
-
-    # solutions = numpy.random.permutation(solutions)
-
-    # print("Finish to permutate", f"{time.time() - start} seconds")
-
-    # new_solutions = []
-    #
-    # start = time.time()
-
-    # print("Comparing...")
-
-    # for i in range(0, len(solutions), 2):
-    #     s1, s2 = solutions[i], solutions[i + 1]
-    #     s1.objective_function()
-    #     s2.objective_function()
-    #     if s1.penal_fitness <= s2.penal_fitness:
-    #         new_solutions.append(s1)
-    #     else:
-    #         new_solutions.append(s2)
-    #
-    # solutions = new_solutions
-    #
-    # print("Final solution", min([m.penal_fitness for m in solutions]), f"{time.time() - start} seconds")
